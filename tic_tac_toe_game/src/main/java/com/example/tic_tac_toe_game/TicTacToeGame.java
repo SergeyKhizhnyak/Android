9b49b2f237212f0fac9f87defab7b9df8aa9a776
SergeyKhizhnyak <sergeykhizhnyak1998@gmail.com>
2022-05-30 14:38:27 +0300

    Add new methods

--- tic_tac_toe_game/src/main/java/com/example/tic_tac_toe_game/TicTacToeGame.java
+++ tic_tac_toe_game/src/main/java/com/example/tic_tac_toe_game/TicTacToeGame.java
@@ -1,68 +1,170 @@
 package com.example.tic_tac_toe_game;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Paint;
-import android.widget.TextView;
+import android.os.Handler;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class TicTacToeGame {
-    private final static int PLAYER_TURN = Paint.UNDERLINE_TEXT_FLAG;
     private final Context context;
     private final Determiner determiner;
+    private final Grid grid;
     private final Player playerOne;
     private final Player playerTwo;
-    private final TextView gameScore;
 
     public TicTacToeGame(
             Context context,
             Determiner determiner,
+            Grid grid,
             Player playerOne,
-            Player playerTwo,
-            TextView gameScore) {
+            Player playerTwo) {
         this.context = context;
         this.determiner = determiner;
+        this.grid = grid;
         this.playerOne = playerOne;
         this.playerTwo = playerTwo;
-        this.gameScore = gameScore;
     }
 
-    public void newGame(Grid grid) {
+    public void makeTurn(ImageView[][] imageViews, ImageView image) {
+        image.setOnClickListener(view -> {
+            if (image.getTag() != null) {
+                return;
+            }
+
+            Player player = determiner.turnDeterminer();
+            player.makeTurn(image);
+
+            if (isWin(imageViews, player)) {
+                winCase(player);
+            }
+        });
+    }
+
+    public void newGame() {
         if (grid.isGridEmpty()) {
             return;
         }
 
-        grid.clearGrid();
-
-        if (playerOne.getTurn() == PLAYER_TURN) {
-            playerOne.setTurn(0);
+        if (playerOne.isTurn()) {
             playerTwo.setCount(playerTwo.getCount() + 1);
+            playerOne.setTurnFalse();
         } else {
-            playerTwo.setTurn(0);
             playerOne.setCount(playerOne.getCount() + 1);
+            playerTwo.setTurnFalse();
         }
 
         determiner.sideDeterminer();
-        refreshScore();
+        grid.clearGrid();
     }
 
     public void resetGame(Intent intent) {
+        if (grid.isGridEmpty()) {
+            if (playerOne.getCount() == 0 && playerTwo.getCount() == 0) {
+                return;
+            }
+        }
+
         ((Activity) context).finish();
         context.startActivity(intent);
         ((Activity) context).overridePendingTransition(0, 0);
     }
 
-    public void inWinCase(Player player) {
-        player.setCount(player.getCount() + 1);
-        Toast.makeText(context, player.getName() + " wins", Toast.LENGTH_SHORT).show();
-        refreshScore();
-        determiner.sideDeterminer();
+    private boolean isWin(ImageView[][] grid, Player player) {
+        int side = player.getSide();
+
+        if (rowsForWinCheck(grid, side)) {
+            return true;
+        }
+
+        if (columnsForWinCheck(grid, side)) {
+            return true;
+        }
+
+        if (leftDiagonalForWinCheck(grid, side)) {
+            return true;
+        }
+
+        return rightDiagonalForWinCheck(grid, side);
     }
 
-    @SuppressLint("SetTextI18n")
-    private void refreshScore() {
-        gameScore.setText(playerOne.getCount() + ":" + playerTwo.getCount());
+    public void winCase(Player player) {
+        Toast.makeText(context, player.getName() + " wins", Toast.LENGTH_SHORT).show();
+        player.setCount(player.getCount() + 1);
+        determiner.sideDeterminer();
+        new Handler().postDelayed(grid::clearGrid, 2000);
+    }
+
+    private boolean rowsForWinCheck(ImageView[][] grid, int side) {
+        for (int x = 0; x < grid.length; x++) {
+            int counter = 0;
+
+            for (ImageView[] row : grid) {
+                try {
+                    if (row[x].getTag().equals(side)) {
+                        counter += 1;
+                    }
+                } catch (NullPointerException exception) {
+                    break;
+                }
+            }
+
+            if (counter == grid.length) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean columnsForWinCheck(ImageView[][] grid, int side) {
+        for (ImageView[] column : grid) {
+            int counter = 0;
+
+            for (int y = 0; y < grid.length; y++) {
+                try {
+                    if (column[y].getTag().equals(side)) {
+                        counter += 1;
+                    }
+                } catch (NullPointerException exception) {
+                    break;
+                }
+            }
+
+            if (counter == grid.length) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean leftDiagonalForWinCheck(ImageView[][] grid, int side) {
+        for (int i = 0; i < grid.length; i++) {
+            try {
+                if (!grid[i][i].getTag().equals(side)) {
+                    return false;
+                }
+            } catch (NullPointerException exception) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private boolean rightDiagonalForWinCheck(ImageView[][] grid, int side) {
+        for (int x = 0; x < grid.length; x++) {
+            try {
+                if (!grid[x][grid.length - 1 - x].getTag().equals(side)) {
+                    return false;
+                }
+            } catch (NullPointerException exception) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
