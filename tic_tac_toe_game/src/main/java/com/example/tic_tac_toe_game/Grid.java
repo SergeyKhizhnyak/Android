179cb0c8876dcc44f036d0c8b5eb30f8ff4327c2
SergeyKhizhnyak <sergeykhizhnyak1998@gmail.com>
2022-05-30 14:47:21 +0300

    Remove method, add new methods

--- tic_tac_toe_game/src/main/java/com/example/tic_tac_toe_game/Grid.java
+++ tic_tac_toe_game/src/main/java/com/example/tic_tac_toe_game/Grid.java
@@ -1,62 +1,14 @@
 package com.example.tic_tac_toe_game;
 
-import android.content.Context;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TextView;
 
 public class Grid {
     private final ImageView[][] grid = new ImageView[3][3];
 
-    public void makeGrid(
-            LinearLayout linearLayout,
-            Context context,
-            Player playerOne,
-            Player playerTwo,
-            Determiner determiner,
-            TextView gameScore) {
-        for (int y = 0; y < 3; y++) {
-            LinearLayout row = new LinearLayout(context);
-            row.setLayoutParams(new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    1));
-            row.setWeightSum(3);
-
-            for (int x = 0; x < 3; x++) {
-                ImageView image = new ImageView(context);
-                LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        LinearLayout.LayoutParams.MATCH_PARENT,
-                        1);
-                layoutParams.setMargins(20, 20, 20, 20);
-                grid[x][y] = image;
-                row.addView(image, layoutParams);
-                image.setOnClickListener(view -> {
-                    if (image.getDrawable() != null) {
-                        return;
-                    }
-
-                    Player player = determiner.turnDeterminer();
-                    image.setTag(player.getSide());
-                    player.makeTurn(image);
-
-                    if (determiner.winDeterminer(grid, player)) {
-                        TicTacToeGame ticTacToeGame = new TicTacToeGame(context, determiner, playerOne, playerTwo, gameScore);
-                        ticTacToeGame.inWinCase(player);
-                        clearGrid();
-                    }
-                });
-            }
-
-            linearLayout.addView(row);
-        }
-    }
-
     public boolean isGridEmpty() {
-        for (ImageView[] row : grid) {
-            for (ImageView image : row) {
-                if (image.getDrawable() != null) {
+        for (int x = 0; x < grid.length; x++) {
+            for (ImageView[] row : grid) {
+                if (row[x].getDrawable() != null) {
                     return false;
                 }
             }
@@ -66,11 +18,19 @@ public class Grid {
     }
 
     public void clearGrid() {
-        for (ImageView[] row : grid) {
-            for (ImageView image : row) {
-                image.setImageDrawable(null);
-                image.setTag(null);
+        for (int x = 0; x < grid.length; x++) {
+            for (ImageView[] row : grid) {
+                row[x].setImageDrawable(null);
+                row[x].setTag(null);
             }
         }
+    }
+
+    public ImageView[][] getGrid() {
+        return grid;
+    }
+
+    public void setGridElement(int x, int y, ImageView image) {
+        grid[x][y] = image;
     }
 }
